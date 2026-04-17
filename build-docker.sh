cdb4b99fa0bcd7cef114d75b56a9c9a7bff4065a
HUANG, C.-H. <2898938+huangch@users.noreply.github.com>
2026-04-17 09:00:14 -0700

    modified:   build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 docker build --build-arg UID=$(id -u) --build-arg GID=$(id -g) -f ./Dockerfile -t wsinsight:latest .
-docker tag wsinsight:latest https://hub.docker.com/r/huangchtw/wsinsight:latest
-docker push https://hub.docker.com/r/huangchtw/wsinsight:latest
+docker tag wsinsight:latest hub.docker.com/r/huangchtw/wsinsight:latest
+docker push hub.docker.com/r/huangchtw/wsinsight:latest
