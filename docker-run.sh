c5f5b20f2e39ededcb59a62802347438e7d0b3e8
HUANG, C.-H. <2898938+huangch@users.noreply.github.com>
2026-04-17 23:28:42 -0700

    modified:   docker-run.sh

--- docker-run.sh
+++ docker-run.sh
@@ -7,7 +7,6 @@ if [ ! "${1}" = "" ] && [ ! "${2}" = "" ]; then
     echo docker run --rm -it --gpus "device=${2}" --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
     docker run --rm -it --gpus "device=${2}" --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
 elif [ ! "${1}" = "" ]; then
-    # docker run --rm -it --gpus all --shm-size=16g -v "${1}":/workspace ${IMAGE_ID}
     echo docker run --rm -it --gpus all --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
     docker run --rm -it --gpus all --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
 else
