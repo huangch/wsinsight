a4717587b3da784949220f93f4d3cc19431a8b91
HUANG, C.-H. <2898938+huangch@users.noreply.github.com>
2026-04-17 23:24:16 -0700

    modified:   docker-build-push.sh 	modified:   docker-run.sh 	modified:   tmux-multi-gpu.sh

--- docker-run.sh
+++ docker-run.sh
@@ -3,10 +3,10 @@
 IMAGE_ID=huangchtw/wsinsight:latest
 docker pull ${IMAGE_ID}
 
-if [ ! "${1}" == "" ] && [ ! "${2}" == "" ]; then
+if [ ! "${1}" = "" ] && [ ! "${2}" = "" ]; then
 echo docker run --rm -it --gpus "device=${2}" --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
 docker run --rm -it --gpus "device=${2}" --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
-elif [ ! "${1}" == "" ]; then
+elif [ ! "${1}" = "" ]; then
 # docker run --rm -it --gpus all --shm-size=16g -v "${1}":/workspace ${IMAGE_ID}
 echo docker run --rm -it --gpus all --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
 docker run --rm -it --gpus all --shm-size=32g --init -v "${1}":/workspace ${IMAGE_ID}
