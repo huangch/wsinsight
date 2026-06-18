709defbfc229ee5a641faa584efe094a024f1348
huangch <2898938+huangch@users.noreply.github.com>
2026-06-18 16:34:40 -0700

    modified:   wsinsight/_version.py

--- wsinsight/_version.py
+++ wsinsight/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.1.dev357+gb23e96827.d20260428'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev357', 'gb23e96827.d20260428')
+__version__ = version = '0.1.1.dev379+gd0f3012bb.d20260618'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev379', 'gd0f3012bb.d20260618')
 
-__commit_id__ = commit_id = 'gb23e96827'
+__commit_id__ = commit_id = 'gd0f3012bb'
