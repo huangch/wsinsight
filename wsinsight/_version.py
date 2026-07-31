1113abceb494e70f8e6fb518060001612f158c77
huangch <2898938+huangch@users.noreply.github.com>
2026-07-31 10:58:59 -0700

    deleted:    12 	modified:   wsinsight/_version.py

--- wsinsight/_version.py
+++ wsinsight/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '0.1.1.dev385+gb91aa2684.d20260627'
-__version_tuple__ = version_tuple = (0, 1, 1, 'dev385', 'gb91aa2684.d20260627')
+__version__ = version = '0.1.1.dev436+ga5c18868e.d20260731'
+__version_tuple__ = version_tuple = (0, 1, 1, 'dev436', 'ga5c18868e.d20260731')
 
-__commit_id__ = commit_id = 'gb91aa2684'
+__commit_id__ = commit_id = 'ga5c18868e'
